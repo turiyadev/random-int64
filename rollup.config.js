e99bc9b17fac947d85f82e982d496d462ab41a0d
turiyadev <130711224+turiyadev@users.noreply.github.com>
2023-06-09 00:15:16 -0600

    Typescript (#3)

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,29 @@
-export default {
-  input: 'src/RandomInt64.js',
-  preserveEntrySignatures: 'strict',
-  output: [
-    {
-      file: 'dist/random-int64.mjs',
-      format: 'esm'
-    },
-    {
-      file: 'dist/random-int64.cjs',
-      format: 'cjs'
+import dts from "rollup-plugin-dts"
+import esbuild from "rollup-plugin-esbuild"
+
+export default [
+  {
+    input: "src/RandomInt64.ts",
+    plugins: [esbuild()],
+    output: [
+      {
+        file: "dist/random-int64.mjs",
+        format: "esm",
+        sourcemap: true
+      },
+      {
+        file: "dist/random-int64.cjs",
+        format: "cjs",
+        sourcemap: true
+      }
+    ]
+  },
+  {
+    plugins: [dts()],
+    input: "src/RandomInt64.ts",
+    output: {
+      file: "dist/random-int64.d.ts",
+      format: "esm"
     }
-  ]
-};
+  }
+];
--- src/RandomInt64.ts
+++ src/RandomInt64.ts
@@ -1,24 +1,24 @@
 export default class RandomInt64 {
-  #state;
-  #output;
+  #state: Uint32Array;
+  #output: BigUint64Array | BigInt64Array;
 
-  constructor(signed) {
+  constructor(signed?: boolean) {
     this.#state = new Uint32Array([this.#random32(), 0]);
     this.#output = signed
       ? new BigInt64Array(this.#state.buffer)
       : new BigUint64Array(this.#state.buffer);
   }
 
-  #random32() {
+  #random32(): number {
     return Math.floor(Math.random() * 0x100000000);
   }
 
-  #timing32() {
+  #timing32(): number {
     const t = performance.now();
     return Math.floor(t * 0x100000000) ^ Math.floor(t);
   }
 
-  create() {
+  create(): bigint {
     this.#state[0] = this.#random32() ^ this.#state[1];
     this.#state[1] = this.#timing32() ^ this.#state[0];
     return this.#output[0];
